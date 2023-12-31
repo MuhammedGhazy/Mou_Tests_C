cad94688834ccf121854992508cf1993e86938cc
Your Name <you@example.com>
2023-12-31 03:30:02 -0600

    structure

---
+++ 0x02/struct.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
+
+
+struct stduent_type {
+	unsigned short student_id;
+	char student_name [30];
+	float student_degree;
+};
+
+struct student_type mohamed,ibrahim;
+struct student student_1;
+
+
+int main ()
+{
+	struct student_type ahmed;
+	struct student_type ali;
+	struct student_type sayd,ramy;
+	struct student student_2;
+	struct student_1 {
+		unsigned short id;
+		char name [30];
+		float degree;
+	};
+	struct student_1 ahmed;
+
+	return (0);
+}
+
+void func_1 (void)
+{
+	struct student_type mostafa;
+	/* Error this local structure type*/
+//	struct student_1 karem;
+}
---
+++ 0x02/types.h
@@ -0,0 +1,10 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+struct stduent {
+        unsigned short student_id;
+        char student_name [30];
+        float student_degree;
+};
+
+#endif

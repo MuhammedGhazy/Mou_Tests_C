3e3eb4c39c61c3635e4e6cd378cc4e5eb6ff57a3
Your Name <you@example.com>
2024-01-11 20:31:28 +0200

    struct

--- 0x03/2-struct.c
+++ 0x03/2-struct.c
@@ -11,9 +11,11 @@ typedef struct {
 
 student_t Ahmed = {"Ahmed Ibrahim", 22, 2.3};
 student_t *ptr = NULL;
+student_t ismaeil;
 
 void print_student (student_t *ptr_stu);
 void print_student_data (student_t student);
+void get_student_data(student_t *student_info);
 
 int main()
 {
@@ -24,11 +26,16 @@ int main()
 		ptr->student_id = 11;
 		ptr->student_degree = 3.4;
 
-		print_student(ptr);
-		printf("===================\n");
-		print_student_data(Ahmed);
-		printf("===================\n");
-		print_student(&Ahmed);
+	//	print_student(ptr);
+	//	printf("===================\n");
+	//	print_student_data(Ahmed);
+	//	printf("===================\n");
+	//	print_student(&Ahmed);
+		printf("====================\n");
+		get_student_data(&ismaeil);
+		printf("====================\n");
+		print_student(&ismaeil);
+
 	}
 	else
 	{
@@ -47,6 +54,17 @@ void print_student (student_t *ptr_stu)
         printf("Student ID is     : %i \n", ptr_stu->student_id);
         printf("Student Degree is : %0.2f \n", ptr_stu->student_degree);
 }
+
+void get_student_data(student_t *student_info)
+{
+	printf("Please Enter Student Name : \n");
+	gets(&(student_info->student_name));
+	printf("Please Enter Student Id   : \n");
+        scanf("%u", &(student_info->student_id));
+        printf("Please Enter Student Degree: \n");
+        scanf("%f", &(student_info->student_degree));
+}
+
 
 /*this function is not recomnded because the copy data in the stacke and open more frames
  *  without refrance make error in the memory.

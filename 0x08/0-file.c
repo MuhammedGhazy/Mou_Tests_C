57924579ad5398105845e84e60f1b24efd92e5cd
Muhammed Ghazy <ghazymuhammed00@gmail.com>
2024-01-19 17:29:53 +0200

    i/o

--- 0x08/0-file.c
+++ 0x08/0-file.c
@@ -15,7 +15,10 @@ typedef struct
 
 FILE *ptr = NULL;
 student_info student;
-uint8 read_student_data [90];
+uint8 read_student_data [64];
+
+void fetch_student_data (student_info *ptr_student, uint8 *student_data);
+void print_studen_info(student_info *ptr_student_t);
 
 int main()
 {
@@ -25,6 +28,13 @@ int main()
 	if (NULL != ptr)
 	{
 		printf("Okay, I Can Read The File\n");
+		fgets(read_student_data, sizeof(read_student_data),ptr);
+		printf("Data is : %s\n", read_student_data);
+		printf("Data is : %c\n", read_student_data[36]);
+		printf("Data is : %c\n", read_student_data[37]);
+		printf("===============================\n");
+		fetch_student_data(&student, read_student_data);
+		print_studen_info(&student);
 
 
 
@@ -36,7 +46,7 @@ int main()
 	fclose_ret = fclose(ptr);
 	if (FILE_CLOSED_SUCCESSFULY == fclose_ret)
 	{
-		printf("File Closed Successfuly\n");
+		printf("File Closed Successfuly-EOF.\n");
 	}
 	else
 	{
@@ -44,4 +54,29 @@ int main()
 	}
 	return (0);
 }
+void fetch_student_data (student_info *ptr_student, uint8 *student_data)
+{
+	uint8 student_id_str [3] = {0};
+	uint8 student_name_str [14] = {0};
+	uint8 student_degree_str [4] = {0};
 
+	strncpy(student_id_str, student_data + 36, 2);
+	printf("=> Id is %s\n", student_id_str);
+	strncpy(student_name_str, student_data + 11, 14);
+	printf("=> Name is %s\n", student_name_str);
+	strncpy(student_degree_str, student_data + 53, 3);
+	printf("=> Degree is %s\n", student_degree_str);
+
+	ptr_student->student_id = atoi (student_id_str);
+	ptr_student->student_degree = atof (student_degree_str);
+	strcpy(ptr_student->student_name, student_name_str);
+
+}
+void print_studen_info(student_info *ptr_student_t)
+{
+	printf("=========================\n");
+	printf("Student Name Is   : %s\n", ptr_student_t->student_name);
+	printf("Student ID Is     : %i\n", ptr_student_t->student_id);
+	printf("Student Degree Is : %0.2f\n", ptr_student_t->student_degree);
+	printf("==========================\n");
+}
